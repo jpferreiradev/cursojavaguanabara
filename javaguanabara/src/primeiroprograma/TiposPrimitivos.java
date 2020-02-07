878b298dd1c50a02d38dc70dfab2536709c2935c
jotepauder <jotepauder01@gmail.com>
2020-02-07 15:11:29 -0300

    Tipos primitivos e um pouco de OO

---
+++ javaguanabara/src/primeiroprograma/TiposPrimitivos.java
@@ -0,0 +1,35 @@
+package primeiroprograma;
+
+import java.util.Scanner;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+			
+	 int idade = 28;
+	 char nome = 'j';  
+	 boolean meunome = true;
+	 float idadeEsseAno = 28.2f;
+	 String nomecompleto = "João Paulo";
+	 
+	 
+	 System.out.println("Qual é o seu nome completo: " + nomecompleto);
+	 System.out.println("Qual é a sua idade: " + idade + " anos");
+	 //System.out.println("Qual é a primeira letra do seu nome: " + nome);
+	 System.out.println("Qual é a sua idade esse ano: " + idadeEsseAno);
+	
+	 if(meunome == true) {
+		 System.out.println("Qual é a primeira letra do seu nome: " + nome);
+	 } else {
+		 System.out.println("Você errou na primeira letra do seu nome");
+	 }
+		 
+	 Scanner teclado = new Scanner(System.in);
+	 System.out.print("Digite o seu nome completo:" );
+	 nomecompleto = teclado.nextLine();
+	 System.out.print("Digite a sua idade: ");
+	 idade = teclado.nextInt();
+	 System.out.println("O seu nome completo é " + nomecompleto + " e a sua idade é " + idade + " anos");
+	  
+	 
+	}	
+}
