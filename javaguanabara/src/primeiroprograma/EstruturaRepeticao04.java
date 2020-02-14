f3e9cf5efea6f339326c531e9e9d73b964de2801
jotepauder <jotepauder01@gmail.com>
2020-02-14 14:21:21 -0300

    Estruturas de repetição Java

---
+++ javaguanabara/src/primeiroprograma/EstruturaRepeticao04.java
@@ -0,0 +1,24 @@
+package primeiroprograma;
+
+public class EstruturaRepeticao04 {
+	
+	public static void main(String[] args) {
+		//Estrutura repetição com para
+		
+		for(int contador=0;contador<=100;contador+=10) {
+			System.out.println(contador);
+		}
+	}
+}
+
+
+/* Estrutura de repetição com o enquanto
+ * 
+ * int contador=0;
+ * while (contador<4) {
+ * System.out.println("Cambalhota");
+ * contador++;
+ 
+ }
+ */
+ 
