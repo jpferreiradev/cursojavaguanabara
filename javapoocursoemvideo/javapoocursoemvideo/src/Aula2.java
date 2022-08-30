10890a416146be9bec8982d2ba2f644e17af5909
JP Ferreira <jotepauder01@gmail.com>
2022-08-30 17:12:40 -0300

    Curso POO - Curso em Video

--- javapoocursoemvideo/javapoocursoemvideo/src/Aula2.java
+++ javapoocursoemvideo/javapoocursoemvideo/src/Aula2.java
@@ -5,12 +5,20 @@ public class Aula2 {
 
         c1.cor = "Azul";
         c1.ponta = 0.5;
-        c1.destampar();
 
+        c1.tampar();
         c1.status();
         c1.rabiscar();
-        }
 
+        Caneta c2 = new Caneta();
+
+        c2.modelo = "Playstation";
+        c2.cor = "Preto";
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
+
+    }
 }
 
 
