251965793082d4e159956760cf57835a57a1490e
JP Ferreira <jotepauder01@gmail.com>
2022-09-05 19:10:29 -0300

    Update curso POO

---
+++ javapoocursoemvideo/javapoocursoemvideo/src/Aula04/Aula04.java
@@ -0,0 +1,24 @@
+package Aula04;
+
+public class Aula04 {
+    public static void main(String args[]){
+
+        Caneta c1 = new Caneta("Nic", "Amarela", 0.4);
+        c1.status();
+
+        Caneta c2 = new Caneta("KKK", "Laranja", 1.0);
+        c2.status();
+    }
+}
+
+/*
+
+c1.setModelo("Bic");
+//c1.modelo = "Bic"; // Acessando diretamente pelo atributo
+c1.setPonta(0.5);
+//c1.ponta = 0.5; // Acessando diretamente pelo atributo
+
+        System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
+
+        //System.out.println("O tamanho da ponta é: " + c1.getPonta());
+ */
---
+++ javapoocursoemvideo/javapoocursoemvideo/src/Aula04/Caneta.java
@@ -0,0 +1,54 @@
+package Aula04;
+
+public class Caneta {
+    // Aqui estão os seus atributos
+    private String modelo;
+    private double ponta;
+    private boolean tampada;
+    private String cor;
+    // Aqui estão os seus métodos construtores
+
+    public Caneta(String m, String c, double p){ // Este é o método construtor
+        this.modelo = m;
+        this.cor = c;
+        this.setPonta (p);
+        this.tampar();
+
+    }
+
+    // Aqui estão os seus métodos
+
+    public String getModelo() {
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    public double getPonta(){
+        return this.ponta;
+    }
+
+    public void setPonta(double p) {
+        this.ponta = p;
+
+    }
+
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
+    }
+
+    public void status(){
+        System.out.println("Sobre a caneta");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor:" + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+
+
+    }
+
+}
